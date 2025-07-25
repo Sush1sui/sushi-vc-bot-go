6e311274af7f4491632c8d16f7a3b363bcc745fa
Sush1sui <jplovesraw@gmail.com>
2025-07-25 15:35:00 +0800

    feat: transfer ownership

---
+++ internal/bot/button/transferOwner.go
@@ -0,0 +1,182 @@
+package button
+
+import (
+	"fmt"
+
+	"github.com/Sush1sui/sushi-vc-bot-go/internal/bot"
+	"github.com/Sush1sui/sushi-vc-bot-go/internal/repository"
+	"github.com/bwmarrin/discordgo"
+)
+
+func TransferOwnership(i *discordgo.InteractionCreate) {
+	if i.Member == nil || i.GuildID == "" { return }
+
+	res, err := repository.CustomVcService.GetByOwnerOrChannelId(i.Member.User.ID, "")
+	if err != nil || res == nil {
+		err := bot.Session.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "You do not own any voice channel.",
+				Flags:   discordgo.MessageFlagsEphemeral,
+			},
+		})
+		if err != nil {
+			fmt.Println("Error responding to interaction:", err)
+		}
+		return
+	}
+
+	minValue := 1
+	selectMenu := discordgo.SelectMenu{
+		MenuType:    discordgo.UserSelectMenu,
+		CustomID:   "transfer_ownership_menu",
+		Placeholder: "Select a user to transfer ownership",
+		MinValues:   &minValue,
+		MaxValues:   1,
+	}
+
+	row := discordgo.ActionsRow{Components: []discordgo.MessageComponent{selectMenu}}
+
+	err = bot.Session.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseModal,
+		Data: &discordgo.InteractionResponseData{
+			Title:    "Transfer Ownership",
+			Components: []discordgo.MessageComponent{row},
+		},
+	})
+	if err != nil {
+		e := bot.Session.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Failed to create transfer ownership menu.",
+				Flags:   discordgo.MessageFlagsEphemeral,
+			},
+		})
+		if e != nil {
+			fmt.Println("Error responding to interaction:", e)
+		}
+		return
+	}
+}
+
+func HandleTransferOwnership(i *discordgo.InteractionCreate) {
+	if i.GuildID == "" || i.Member == nil { return }
+
+	res, err := repository.CustomVcService.GetByOwnerOrChannelId(i.Member.User.ID, "")
+	if err != nil || res == nil {
+		err := bot.Session.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "You do not own a custom voice channel.",
+				Flags:   discordgo.MessageFlagsEphemeral,
+			},
+		})
+		if err != nil {
+			fmt.Println("Error responding to interaction:", err)
+		}
+		return
+	}
+
+	customVc, err := bot.Session.Channel(res.ChannelID)
+	if err != nil || customVc == nil {
+		err = bot.Session.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Failed to retrieve custom VC channel.",
+				Flags:   discordgo.MessageFlagsEphemeral,
+			},
+		})
+		if err != nil {
+			fmt.Println("Error responding to interaction:", err)
+		}
+		return
+	}
+	if customVc.OwnerID != i.Member.User.ID {
+		err = bot.Session.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "You do not own this voice channel.",
+				Flags:   discordgo.MessageFlagsEphemeral,
+			},
+		})
+		if err != nil {
+			fmt.Println("Error responding to interaction:", err)
+		}
+		return
+	}
+
+	selectedUserId := i.MessageComponentData().Values[0]
+	if selectedUserId == "" || len(i.MessageComponentData().Values) == 0 {
+		err := bot.Session.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "No user selected.",
+				Flags:   discordgo.MessageFlagsEphemeral,
+			},
+		})
+		if err != nil {
+			fmt.Println("Error responding to interaction:", err)
+		}
+		return
+	}
+	
+	// Set the new owner
+	err = bot.Session.ChannelPermissionSet(
+		customVc.ID,
+		selectedUserId,
+		discordgo.PermissionOverwriteTypeMember,
+		discordgo.PermissionViewChannel | discordgo.PermissionManageChannels | discordgo.PermissionVoiceMoveMembers | discordgo.PermissionSendMessages | discordgo.PermissionAddReactions | discordgo.PermissionAttachFiles | discordgo.PermissionReadMessageHistory | discordgo.PermissionVoiceConnect,
+		0,
+	)
+	if err != nil {
+		e := bot.Session.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: fmt.Sprintf("Failed to set permissions for <@%s> in the custom VC.", selectedUserId),
+				Flags:   discordgo.MessageFlagsEphemeral,
+			},
+		})
+		if e != nil {
+			fmt.Println("Error responding to interaction:", e)
+		}
+		return
+	}
+
+	// Remove permissions for the old owner
+	err = bot.Session.ChannelPermissionSet(
+		customVc.ID,
+		i.Member.User.ID,
+		discordgo.PermissionOverwriteTypeMember,
+		0,
+		discordgo.PermissionManageChannels | discordgo.PermissionVoiceMoveMembers | discordgo.PermissionAddReactions | discordgo.PermissionAttachFiles,
+	)
+	if err != nil {
+		e := bot.Session.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Failed to remove permissions for the old owner in the custom VC.",
+				Flags:   discordgo.MessageFlagsEphemeral,
+			},
+		})
+		if e != nil {
+			fmt.Println("Error responding to interaction:", e)
+		}
+		return
+	}
+
+	// Change the owner in the database
+	count, err := repository.CustomVcService.ChangeOwnerByChannelId(res.ChannelID, selectedUserId)
+	if err != nil || count == 0 {
+		e := bot.Session.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Failed to transfer ownership of the custom VC in the database, contact your developer.",
+				Flags:   discordgo.MessageFlagsEphemeral,
+			},
+		})
+		if e != nil {
+			fmt.Println("Error responding to interaction:", e)
+		}
+		return
+	}
+}
